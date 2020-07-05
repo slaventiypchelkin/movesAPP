f832543e278762028c7dd7d9c2c1d1c28bafa0b7
Slaventiy <slaventiypchelkin@gmail.com>
2020-07-05 21:12:46 +0300

    added variables, asked user about movies

--- script/script.js
+++ script/script.js
@@ -1,10 +1,18 @@
 'use strict'
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 let personaMovieDB = {
     count  : numberOfFilms,
     moves  : {},
     actors : {},
     genres : [],
     privat : false
-}
+};
+const a = prompt("Один из последних фильмов, которые вы посмотрели?", ''),
+      b = prompt('На сколько оцените фильм?', ''),
+      c = prompt("Один из последних фильмов, которые вы посмотрели?", ''),
+      d = prompt('На сколько оцените фильм?', '');
+
+personaMovieDB.moves[a] = b;
+personaMovieDB.moves[c] = d;
+console.log(personaMovieDB);
