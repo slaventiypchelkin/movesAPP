ac195d2d430b2b3d68a8111812492fe4a8e3137c
Slaventiy <slaventiypchelkin@gmail.com>
2020-07-06 00:50:44 +0300

    added a loop and checking the number of films watched.

--- script/script.js
+++ script/script.js
@@ -8,11 +8,24 @@ let personaMovieDB = {
     genres : [],
     privat : false
 };
-const a = prompt("Один из последних фильмов, которые вы посмотрели?", ''),
-      b = prompt('На сколько оцените фильм?', ''),
-      c = prompt("Один из последних фильмов, которые вы посмотрели?", ''),
-      d = prompt('На сколько оцените фильм?', '');
 
-personaMovieDB.moves[a] = b;
-personaMovieDB.moves[c] = d;
+
+for (let i = 0; i < 2; i++){
+    const a = prompt("Один из последних фильмов, которые вы посмотрели?", ''),
+          b = prompt('На сколько оцените фильм?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        personaMovieDB.moves[a] = b;
+        console.log('Done!');
+    }else{
+        console.log('Error');
+        i--;
+    }        
+}
+if (personaMovieDB.count < 10){
+    console.log('Вы смотрите очень мало фильмов');
+}else if(personaMovieDB.count >= 10 && personaMovieDB.count < 30){
+    console.log('Нелохой результат по просмотрам, так держать!');
+}else if(personaMovieDB.count >= 30){
+    console.log('Да вы прожженный кинолюбитель! Wow!');
+}else console.log('Что-то пошло не так')
 console.log(personaMovieDB);
